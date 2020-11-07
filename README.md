0e0206de00c78b2be0e5458f4245d8e604afe471
root <no-reply@kamakepar.rugit config --global user.name Server>
2020-11-07 16:07:08 +0000

    Newman

--- README.md
+++ README.md
@@ -16,3 +16,5 @@
 ## It is a project to scan all net
 # Allnetwork
 ## It is a project to scan all net
+# Allnetwork
+## It is a project to scan all net
--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 echo "# Allnetwork" >> README.md
 echo "## It is a project to scan all net">> README.md
+git config --global user.email "no-reply@kamakepar.ru
+git config --global user.name "Server Transformer & Uploader
 git init
 git add *
 git commit -m 'Newman'
