9232ef964f5c542460ce8fa1a550e58f0cbd5fca
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-07 20:24:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ echo "---------------------------- "
 echo "Starting nmap"
 echo "---------------------------- "
 echo '1'>>r.txt 
-nmap -v -iR 100 -Pn -p 80 | grep 'Discovered open port' > lastthing.txt
+nmap -v -iR 10000 -Pn -p 80 | grep 'Discovered open port' > lastthing.txt
 echo "Starting nmap"
 bash init.sh
