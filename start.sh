89be17336647a73234ad8edee7eb6e91ee5cb0ae
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-07 19:47:53 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ sudo apt install nmap -y
 echo "---------------------------- "
 echo "Starting nmap"
 echo "---------------------------- "
-echo '1'>r.txt 
-nmap -v -iR 102400 -Pn -p 80 | grep 'Discovered open port' > lastthing.txt
+echo '1'>>r.txt 
+nmap -v -iR 100 -Pn -p 80 | grep 'Discovered open port' > lastthing.txt
 echo "Starting nmap"
 bash init.sh
