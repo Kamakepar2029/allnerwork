5ae71d1887ddba04863543edd6d43d74e7509172
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-11 19:47:15 +0300

    Update start.py

--- start.py
+++ start.py
@@ -8,4 +8,11 @@ bot = telebot.TeleBot(TOKEN)
 
 
 bot.send_message(id,'Starting Workflow')
+bot.send_message(id,'Starting Engines')
+bot.send_message(id,'Starting Scanning neural net...')
+bot.send_message(id,'Starting Module 1')
+bot.send_message(id,'Starting Module 2')
+bot.send_message(id,'Modules started successfully')
+bot.send_message(id,'Starting Scan')
 
+bot.send_message(id,'Scan Started')
