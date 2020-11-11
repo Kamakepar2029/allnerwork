77a0cff359a5d4898253c1ee17cdc97e6cc5c014
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-11 20:29:55 +0300

    Update end.py

--- end.py
+++ end.py
@@ -9,4 +9,11 @@ bot = telebot.TeleBot(TOKEN)
 bot.send_message(id,'Ending Workflow')
 resu = open("res.txt","r").read()
 bot.send_message(id,'Results of scan Saved: ')
+mass = resu.split('\n')
+start = 0
+end = len(mass)
+while start<end:
+  bot.send_message(id,mass[start])
+  start+=1
+
 bot.send_message(id,resu)
