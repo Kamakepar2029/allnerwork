5af3d1aeae01a564400ef484efdbbf4fd530e62d
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-12 12:24:46 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,9 @@ echo "Starting nmap"
 echo "---------------------------- "
 echo '1'>>r.txt 
 #nmap -v -iR 9000 -Pn -p 80 | grep 'Discovered open port' > res.txt
-nmap -sV -p 80 -v -n  --script http-title  -iL res.txt
+zmap -p 80 178.248.237.0/5999 -B 100M -o all.txt
+nmap -sV -p 80 -v -n  --script http-title  -all habr.txt>res.txt
+
 echo "Ending nmap"
 python3 end.py
 bash init.sh
