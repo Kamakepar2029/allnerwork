d66e58505b969707563d9e6f5751789b74bcf6d8
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-12 12:33:42 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 sudo apt install nmap -y
+sudo apt install zmap -y
 sudo apt install python3-pip -y
 pip3 install setuptools
 pip3 install telebot pytelegrambotapi
@@ -9,7 +10,7 @@ echo "---------------------------- "
 echo '1'>>r.txt 
 #nmap -v -iR 9000 -Pn -p 80 | grep 'Discovered open port' > res.txt
 zmap -p 80 178.248.237.0/5999 -B 100M -o all.txt
-nmap -sV -p 80 -v -n  --script http-title  -all habr.txt>res.txt
+nmap -sV -p 80 -v -n  --script http-title  -iL habr.txt > res.txt
 
 echo "Ending nmap"
 python3 end.py
